8d3bbcf2a326ca5f57f0d273545d4c23db1f6cb7
Thiago Lima <thiago.lima951@gmail.com>
2023-10-28 03:12:36 -0300

    Implementado paginação via JS

--- AplicacaoRickEMorty/wwwroot/js/site.js
+++ AplicacaoRickEMorty/wwwroot/js/site.js
@@ -2,6 +2,7 @@
     document.getElementById("status").value; // Define "Todos" como o valor padrão
     applyFilters(getPageNumberFromUrl(window.location.href));
 });
+
 function getPageNumberFromUrl(url) {
     var pageNumber = 1; // Define o número da página como 1 por padrão
     var urlParams = new URLSearchParams(new URL(url).search);
@@ -42,11 +43,81 @@ function applyFilters(page) {
         .then(response => response.json())
         .then(data => {
             updateDisplay(data);
+            var paginationContainer = document.getElementById("paginacao");
+            paginationContainer.innerHTML = ""; // Limpa os links de página existentes
+            if (data.info.prev) {
+                var prevPageNumber = extractPageNumber(data.info.prev);
+                if (prevPageNumber) {
+                    paginationContainer.innerHTML += `<div class="d-inline-block mr-3">
+                        <a id="previous-page" href="#" onclick="applyFilters(${prevPageNumber})">Anterior</a>
+                    </div>`;
+                }
+            }
+            if (data.info.pages > 1) {
+                var currentPage = page ? page : 1;
+                var pageCount = data.info.pages;
+
+                if (currentPage >= 5) {
+                    paginationContainer.innerHTML += `<div class="mx-3 d-inline-block">
+                        <a href="#" onclick="applyFilters(${currentPage - 5})">...</a>
+                    </div>`;
+                }
+
+                for (var i = currentPage; i <= currentPage + 4 && i <= pageCount; i++) {
+                    if (i === currentPage) {
+                        paginationContainer.innerHTML += `<div class="mx-3 d-inline-block">
+                            <span>${i}</span>
+                        </div>`;
+                    } else {
+                        paginationContainer.innerHTML += `<div class="mx-3 d-inline-block">
+                            <a href="#" onclick="applyFilters(${i})">${i}</a>
+                        </div>`;
+                    }
+                }
+
+                if (currentPage + 5 <= pageCount) {
+                    paginationContainer.innerHTML += `<div class="mx-3 d-inline-block">
+                        <a href="#" onclick="applyFilters(${currentPage + 5})">...</a>
+                    </div>`;
+                }
+            }
+            if (data.info.next) {
+                var nextPageNumber = extractPageNumber(data.info.next);
+                if (nextPageNumber) {
+                    paginationContainer.innerHTML += `<div class="d-inline-block">
+                        <a id="next-page" href="#" onclick="applyFilters(${nextPageNumber})">Próxima</a>
+                    </div>`;
+                }
+            }
             history.pushState({}, '', '?' + params.toString());
         })
         .catch(error => {
             console.error('Houve um erro ao buscar os personagens:', error);
         });
+}
+
+function extractPageNumber(url) {
+    var urlParams = new URLSearchParams(new URL(url).search);
+    if (urlParams.has('page')) {
+        return parseInt(urlParams.get('page'));
+    }
+    return null;
+}
+
+function extractPageNumber(url) {
+    var urlParams = new URLSearchParams(new URL(url).search);
+    if (urlParams.has('page')) {
+        return parseInt(urlParams.get('page'));
+    }
+    return null;
+}
+
+function extractPageNumber(url) {
+    var urlParams = new URLSearchParams(new URL(url).search);
+    if (urlParams.has('page')) {
+        return parseInt(urlParams.get('page'));
+    }
+    return null;
 }
 function updateDisplay(data) {
     var container = document.querySelector(".row"); // Seletor para o contêiner dos personagens
